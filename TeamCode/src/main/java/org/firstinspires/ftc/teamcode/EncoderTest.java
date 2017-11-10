f33bd6d5eeef449b29257e09ae14fafa13fa1859
Andrew Chen <andrew@Andrews-MacBook-Air.local>
2017-11-09 16:35:57 -0800

    we're all clowns

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
@@ -14,27 +14,32 @@ public class EncoderTest extends LinearOpMode {
         robot.init(hardwareMap);
         waitForStart();
 
-        robot.leftDriveFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.rightDriveFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        while (opModeIsActive())
+        {
 
-        robot.leftDriveFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.rightDriveFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.leftDriveFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightDriveFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        robot.leftDriveFront.setTargetPosition(10);
-        robot.rightDriveFront.setTargetPosition(10);
+            robot.leftDriveFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            robot.rightDriveFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        robot.leftDriveFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.rightDriveFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.leftDriveFront.setTargetPosition(3);
+            robot.rightDriveFront.setTargetPosition(3);
 
-        robot.leftDriveFront.setPower(1);
-        robot.rightDriveFront.setPower(1);
+            robot.leftDriveFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightDriveFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        while (robot.leftDriveFront.isBusy() && robot.rightDriveFront.isBusy()) {
+            robot.leftDriveFront.setPower(1);
+            robot.rightDriveFront.setPower(1);
+
+            while (robot.leftDriveFront.isBusy() && robot.rightDriveFront.isBusy()) {
+            }
 
         }
 
-        robot.leftDriveFront.setPower(0);
-        robot.rightDriveFront.setPower(0);
+
+//        robot.leftDriveFront.setPower(0);
+//        robot.rightDriveFront.setPower(0);
 
 //        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 //        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
